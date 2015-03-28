eeb8508d2dae3fd911bce25d4a393558c689e200
Wayne <wayne.elgin@gmail.com>
2015-03-28 19:48:51 -0400

    Remove intro screen for now

--- Powers Combine/Assets/Scripts/GameManager.cs
+++ Powers Combine/Assets/Scripts/GameManager.cs
@@ -51,7 +51,7 @@ public class GameManager : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		if (!this.gameStarted 
-		    && Input.GetKeyDown (KeyCode.Space)
+//		    && Input.GetKeyDown (KeyCode.Space)
 		    && (Time.time - this.timeSinceLevelChange) > introDelay) {
 			this.startNewLevel();
 		}
