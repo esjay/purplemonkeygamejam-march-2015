e1c7d4636435b02822baec76b327d94b8fec7f2b
Wayne <wayne.elgin@gmail.com>
2015-03-28 18:56:55 -0400

    Merge Conflicts

--- Powers Combine/Assets/Scripts/PlayerScript.cs
+++ Powers Combine/Assets/Scripts/PlayerScript.cs
@@ -16,6 +16,7 @@ public class PlayerScript : MonoBehaviour {
 	public int coolDownDuration = 35;
 	public GameObject fistPrefab;
 	public GameObject laserPrefab;
+	public int bulletSpeed = 300;
 	public AudioClip swingSound;
 	
 	public AudioSource audioSource;
@@ -65,29 +66,40 @@ public class PlayerScript : MonoBehaviour {
 
 	void doActions () {
 //		bool isPunching;
-//		bool isPunchRequested;
+		bool isPunchRequested;
 //		bool isStateChange;
 //
-//		isPunchRequested = Input.GetKeyDown (KeyCode.Space);
-//		if (!this.isPunching && isPunchRequested) {
-//			this.isPunching = true;
+		isPunchRequested = Input.GetKeyDown (KeyCode.Space);
+		if (!this.isPunching && isPunchRequested) {
+			this.isPunching = true;
 //			this.beginPunch ();
-//			this.punchingSince = Time.frameCount;
-//			applyAnimationFromState(true);
-//		}
+			this.punchingSince = Time.frameCount;
+			applyAnimationFromState(true);
+			fire();
+		}
 //
-//		int punchingFor = 0;
-//		if (this.isPunching) {
-//			punchingFor = Time.frameCount - this.punchingSince;
-//		}
+		int punchingFor = 0;
+		if (this.isPunching) {
+			punchingFor = Time.frameCount - this.punchingSince;
+		}
 //
 //		if (punchingFor > this.punchDuration) {
 //			this.endPunch ();
 //			applyAnimationFromState(false);
 //		}
-//		if (punchingFor > this.coolDownDuration) {
-//			this.isPunching = false;
-//		}
+		if (punchingFor > this.coolDownDuration) {
+			this.isPunching = false;
+		}
+	}
+
+	void fire() {
+		Rigidbody2D clone;
+		clone = Instantiate(laserPrefab, transform.position, transform.rotation) as Rigidbody2D;
+		clone.velocity = transform.TransformDirection(Vector3.forward * bulletSpeed);
+//		clone.AddForce(transform.up * bulletSpeed);
+
+//		Rigidbody2D projectileInstance = (Rigidbody2D)Instantiate (laserPrefab, new Vector2(transform.position.x + xValue, transform.position.y + yValue));
+//		projectileInstance.GetComponent<Rigidbody2D>().AddForce(transform.up * bulletSpeed);
 	}
 
 	void beginPunch () {
